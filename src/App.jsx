72149b671aa5dccbb3fb9dffb41446fa9a4ddd96
huzaifaghazali <huzaifaghazali@gmail.com>
2024-01-01 21:06:46 +0500

    Add Row styled component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,40 @@
-import GlobalStyles from "./styles/GlobalStyles";
-import Button from "./ui/Button";
-import Heading from "./ui/Heading";
-import Input from "./ui/Input";
+import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+import Button from './ui/Button';
+import Heading from './ui/Heading';
+import Input from './ui/Input';
+import Row from './ui/Row';
+
+const StyledApp = styled.main`
+  padding: 20px;
+`;
 
 function App() {
-
   return (
     <>
-    <GlobalStyles />
-      <Heading as='h1'>The Wild Oasis</Heading>
-      <Heading as='h2'>Check in and out</Heading>
-      <Button>Hello</Button>
-      <Input />
-      <Heading as='h3'>Form</Heading>
+      <GlobalStyles />
+      <StyledApp>
+        <Row>
+          <Row type='horizontal'>
+            <Heading as='h1'>The Wild Oasis</Heading>
+            <div>
+              <Heading as='h2'>Check in and out</Heading>
+              <Button>Check in</Button>
+              <Button>Check Out</Button>
+            </div>
+          </Row>
+
+          <Row>
+            <Heading as='h3'>Form</Heading>
+            <form>
+              <Input type='number' placeholder='Number of guests' />
+              <Input type='number' placeholder='Number of guests' />
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
