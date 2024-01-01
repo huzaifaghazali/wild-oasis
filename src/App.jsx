29d2be9993b4a48a0fd96c66522ab3ab96e39fbc
huzaifaghazali <huzaifaghazali@gmail.com>
2024-01-01 21:12:40 +0500

    Style buttons

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,9 @@ function App() {
             <div>
               <Heading as='h2'>Check in and out</Heading>
               <Button>Check in</Button>
-              <Button>Check Out</Button>
+              <Button variation='secondary' size='small'>
+                Check Out
+              </Button>
             </div>
           </Row>
 
