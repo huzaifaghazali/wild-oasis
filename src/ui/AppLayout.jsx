ad2e398eb647c7e53687597d27a71247be63c2e2
huzaifaghazali <huzaifaghazali@gmail.com>
2024-01-01 21:55:53 +0500

    Style the AppLayout component

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -2,18 +2,30 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import styled from 'styled-components';
 
 function AppLayout() {
+  const StyledAppLayout = styled.div`
+    display: grid;
+    grid-template-columns: 26rem 1fr;
+    grid-template-rows: auto 1fr;
+    height: 100vh;
 
-   
+  `;
+
+  const Main = styled.main`
+    background-color: var(--color-grey-50);
+    padding: 4rem 4.8rem 6.4rem;
+  `;
+
   return (
-    <div>
+    <StyledAppLayout>
       <Header />
       <Sidebar />
-      <main>
+      <Main>
         <Outlet />
-      </main>
-    </div>
+      </Main>
+    </StyledAppLayout>
   );
 }
 
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+   background-color: var(--color-grey-0);
+   padding: 1.2rem 4.8rem;
+   border-bottom: 1px solid var(--color-grey-100);
+`;
 
 function Header() {
   return (
-    <header>Header</header>
+    <StyledHeader>Header</StyledHeader>
   )
 }
 
