91f4ed393632ef3962a044a8bc7156a07a8e1b20
huzaifaghazali <huzaifaghazali@gmail.com>
2024-01-01 20:47:53 +0500

    Add Heading component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
+import Heading from "./ui/Heading";
 import Input from "./ui/Input";
-
 
 function App() {
 
   return (
     <>
     <GlobalStyles />
+      <Heading as='h1'>The Wild Oasis</Heading>
+      <Heading as='h2'>Check in and out</Heading>
       <Button>Hello</Button>
       <Input />
+      <Heading as='h3'>Form</Heading>
     </>
   )
 }
