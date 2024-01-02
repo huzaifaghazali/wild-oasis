feecb3d0a964d19cb0e28bd95d799265dc543d45
huzaifaghazali <huzaifaghazali@gmail.com>
2024-01-02 21:33:37 +0500

    Get the Cabins data

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,16 @@
-import Heading from "../ui/Heading";
-import Row from "../ui/Row";
+import { useEffect } from 'react';
+import Heading from '../ui/Heading';
+import Row from '../ui/Row';
+import { getCabins } from '../services/apiCabings';
 
 function Dashboard() {
+  useEffect(() => {
+    getCabins().then((data) => console.log(data));
+  }, []);
+
   return (
-    <Row type="horizontal">
-      <Heading as="h1">Dashboard</Heading>
+    <Row type='horizontal'>
+      <Heading as='h1'>Dashboard</Heading>
       <p>TEST</p>
     </Row>
   );
