6880ff4df229a319afde021573c226c07e159c08
huzaifaghazali <huzaifaghazali@gmail.com>
2024-01-02 21:43:28 +0500

    Show image in cabin

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,11 +1,21 @@
-import Heading from "../ui/Heading";
-import Row from "../ui/Row";
+import { useEffect } from 'react';
+import Heading from '../ui/Heading';
+import Row from '../ui/Row';
+import { getCabins } from '../services/apiCabings';
 
 function Cabins() {
+  useEffect(() => {
+    getCabins().then((data) => console.log(data));
+  }, []);
+  
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
+    <Row type='horizontal'>
+      <Heading as='h1'>All cabins</Heading>
       <p>TEST</p>
+      <img
+        src='https://yhfgttihpyarnozxyvsp.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg?t=2024-01-02T16%3A37%3A43.721Z'
+        alt=''
+      />
     </Row>
   );
 }
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,7 @@
-import { useEffect } from 'react';
 import Heading from '../ui/Heading';
 import Row from '../ui/Row';
-import { getCabins } from '../services/apiCabings';
 
 function Dashboard() {
-  useEffect(() => {
-    getCabins().then((data) => console.log(data));
-  }, []);
 
   return (
     <Row type='horizontal'>
