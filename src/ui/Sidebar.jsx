6dad7b64563a8dd72fe935071adbe67c4e6c0c08
huzaifaghazali <huzaifaghazali@gmail.com>
2024-01-02 21:25:17 +0500

    Add Supabase settings

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
+import React from 'react';
 import styled from 'styled-components';
 import Logo from './Logo';
 import MainNav from './MainNav';
 
 const StyledSidebar = styled.aside`
-   background-color: var(--color-grey-0);
-   padding: 3.2rem 2.4rem;
-   border-right: 1px solid var(--color-grey-100);
+  background-color: var(--color-grey-0);
+  padding: 3.2rem 2.4rem;
+  border-right: 1px solid var(--color-grey-100);
 
-   grid-row: 1 / -1;
-   display: flex;
-   flex-direction: column;
-   flex-direction: column;
-   gap: 3.2rem
+  grid-row: 1 / -1;
+  display: flex;
+  flex-direction: column;
+  flex-direction: column;
+  gap: 3.2rem;
 `;
-
 
 function Sidebar() {
   return (
@@ -22,7 +21,7 @@ function Sidebar() {
       <Logo />
       <MainNav />
     </StyledSidebar>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
