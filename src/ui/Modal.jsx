a830347b81d50e2bfe25f73ceeb30a80bc7537a8
huzaifaghazali <huzaifaghazali@gmail.com>
2024-01-16 18:06:43 +0500

    Add style prop on modal

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -86,7 +86,7 @@ function Window({ children, name }) {
         <Button onClick={close}>
           <HiXMark />
         </Button>
-        <div>{children}</div>
+        <div>{cloneElement(children, { onCloseModal: close })}</div>
       </StyledModal>
     </Overlay>,
     document.body
