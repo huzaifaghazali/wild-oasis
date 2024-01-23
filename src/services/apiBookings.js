01865a2199bbcea816c03fead70337962f665065
huzaifaghazali <huzaifaghazali@gmail.com>
2024-01-23 18:25:46 +0500

    Dynamically add pagination count

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -5,24 +5,25 @@ export async function getBookings({ filter, sortBy }) {
   let query = supabase
     .from('bookings')
     .select(
-      'id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)'
+      'id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)',
+      { count: 'exact' }
     );
 
   // FILTER
-  if (filter )
-    query = query[filter.method || 'eq'](filter.field, filter.value);
+  if (filter) query = query[filter.method || 'eq'](filter.field, filter.value);
 
   // SORT
-  if(sortBy) query.order(sortBy.field, { ascending: sortBy.direction === 'asc',});
+  if (sortBy)
+    query.order(sortBy.field, { ascending: sortBy.direction === 'asc' });
 
-  const { data, error } = await query;
+  const { data, error, count } = await query;
 
   if (error) {
     console.error(error);
     throw new Error('Bookings could not be loaded');
   }
 
-  return data;
+  return { data, count };
 }
 
 export async function getBooking(id) {
